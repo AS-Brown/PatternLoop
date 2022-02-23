f715672a8b62b484f3ffc7274474e928d0a7104b
AS-Brown <andrewsbrown@hotmail.co.uk>
2022-02-23 19:56:38 +0000

    adds stars equal to user input

--- src/Pattern.java
+++ src/Pattern.java
@@ -1,15 +1,20 @@
+import java.util.Scanner;
 
 public class Pattern {
 
 	public static void main(String[] args) {
-		for(int i= 1; i <= 5; i++) {
+		System.out.println("Please specify the amount of stars:");
+		Scanner scan = new Scanner(System.in);
+		int numOfStars = scan.nextInt();
+		
+		for(int i= 1; i <= numOfStars; i++) {
 			for(int j = 0; j < i; j++) {
 			System.out.print("*");
 			}
 			System.out.println();
 		}
 		
-		for(int i = 4; i > 0; i--) {
+		for(int i = numOfStars - 1; i > 0; i--) {
 			for(int j = 0; j < i; j++) {
 			System.out.print("*");
 			}
